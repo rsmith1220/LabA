d5609071b5fad9e2994cda68d34aef2f0e6edc1a
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2023-02-24 13:09:16 -0600

    comments and '?'

--- AFNgraph.py
+++ AFNgraph.py
@@ -2,10 +2,12 @@ import graphviz
 
 def create_nfa(postfix_expression):
     dot = graphviz.Digraph(comment='NFA')
-    # Step 1: Recibir y parsear la expresion postfix
+    #Recibir y parsear la expresion postfix
     stack = []
     for token in postfix_expression:
         if token == '*': 
+            #push a la lista STACK
+                        #key                            #value
             state = {'id': len(stack), 'transitions': [(len(stack), len(stack) + 1)], 'accepting': False}
             stack.append(state)
 
@@ -20,13 +22,26 @@ def create_nfa(postfix_expression):
             stack.append(new_state)
 
         elif token == '+':  
+            #hace un pop y una transicion a si mismo
             right_state = stack.pop()
             left_state = {'id': len(stack), 'transitions': [], 'accepting': False}
             left_state['transitions'] += right_state['transitions']
             left_state['transitions'].append((left_state['id'], right_state['id']))
             stack.append(left_state)
 
+        elif token == '?':
+            #hace pop a dos estados
+            #crea un nuevo estado de la izquierda a la derecha
+            right_state = stack.pop()
+            left_state = stack.pop()
+            new_state = {'id': len(stack), 'transitions': [], 'accepting': False}
+            new_state['transitions'] += left_state['transitions']
+            new_state['transitions'] += right_state['transitions']
+            new_state['transitions'].append((left_state['id'], right_state['id'], ''))
+            stack.append(new_state)
+
         elif token == 'ε':
+            #push al siguiente estado
             state = {'id': len(stack), 'transitions': [(len(stack), len(stack) + 1, 'ε')], 'accepting': False}
             stack.append(state)
 
@@ -44,17 +59,14 @@ def create_nfa(postfix_expression):
     # Conecta los estados
     for state in states:
         for transition in state['transitions']:
+            
             if len(transition) == 2:
                 transition += ('',)
             label = transition[2]
+            #con este dot se crea el grafo
             dot.edge(str(state['id']), str(transition[1]), label=label)
 
-    # Step 5: Mark final states as accepting states
-    #Marca los estados finales como estados aceptadores
-    for state in states:
-        if state['id'] == len(states) - 1:
-            state['accepting'] = True
-
+   
     # Retornar 
     #este dot crea el documento para ver el resultado de la maquina
     dot.view()
--- Postix.py
+++ Postix.py
@@ -1,10 +1,9 @@
-from graphviz import Graph
 import AFNgraph
 def Postfix():
-    # Define the operator precedence
+    #Definir un diccionario para saber el orden 
     precedence = {'|': 1, '.': 2, '?': 3, '*': 3, '+': 3}
 
-    # Define the special characters
+    # Define caracteres especiales
     specials = ['|', '.', '?', '*', '+', '(', ')','ε']
 
     def regex_to_postfix(regex):
@@ -12,26 +11,32 @@ def Postfix():
         output = []
         stack = []
         for char in regex:
+            #append a una lista si no es un caracter especial
             if char not in specials:
                 output.append(char)
             elif char == '(':
                 stack.append(char)
             elif char == ')':
+                #si el parentesis no es el que se abre, lo agrega
                 while stack and stack[-1] != '(':
                     output.append(stack.pop())
                 stack.pop()
+                #Si el caracter es un operador, la funcion hace pop
             else:
                 while stack and precedence.get(stack[-1], 0) >= precedence.get(char, 0):
                     output.append(stack.pop())
                 stack.append(char)
+        #append si todavia hay caracteres
         while stack:
             output.append(stack.pop())
+        #regresa el output como un string
         return ''.join(output)
 
-    # Example usage
-    regex = input("Enter a regular expression: ")
+    
+    regex = input("Ingrese su expresion regular: ")
     postfix = regex_to_postfix(regex)
     print(f"Postfix: {postfix}")
+    #se llama la clase AFNgraph para hacer el grafo
     AFNgraph.create_nfa(postfix)
     
 
